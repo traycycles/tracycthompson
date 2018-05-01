3b7beb363fcbe0e073755275ac0cef684fd2bd43
tracy <traymarkthompson@gmail.com>
2018-04-30 21:00:44 -0500

    TODO: Make it purty. Add some content.

--- contact.php
+++ contact.php
@@ -5,104 +5,91 @@ include 'includes/header.php';
 #ini_set("error_reporting", E_ALL);
 #ini_set("display_errors", 1);
 
-$email = $subject= $message= "";
-$emailErr= $subjectErr= $messageErr= "";
-$honey = FALSE;
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["email"])) {
-        $emailErr = "Email is required";
-    } else {
-        $email = test_input($_POST["email"]);
-        //check for email well-formed
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-            $emailErr = "Invalid email format";
-        }
+    $email = trim(filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL));
+    $subject = trim(filter_input(INPUT_POST, "subject", FILTER_SANITIZE_SPECIAL_CHARS));
+    $message = trim(filter_input(INPUT_POST, "message", FILTER_SANITIZE_SPECIAL_CHARS));
+
+    if($email == "" || $subject == "" || $message == ""){
+        $error_message = "Please fill in the required fields: Email, Subject, and Message";
     }
-    if (empty($_POST["subject"])) {
-        $subjectErr = "Subject is required";
-    } else {
-        $subject = test_input($_POST["subject"]);
-        //check for letters and white spaces
-        if(!preg_match("/^[a-zA-Z ]*$/",$subject)){
-            $subjectErr = "Only letters and white space allowed";
-        }
-    }
-    if (empty($_POST["message"])) {
-        $messageErr = "Message is required";
-    } else {
-        $message = test_input($_POST["message"]);
-    }
-    if(!empty($_POST["contact_me_by_fax_only"]) && (bool) $_POST["contact_me_by_fax_only"] == TRUE){
-        $honey = TRUE;
-        header("Location:https//www.google.com");
+    if(!isset($error_message) && $_POST["contact_me_by_fax_only"] != FALSE){
+        header("Location:http://www.google.com");
         exit();
     }
-    $body = array('email' => $email, 'subject' => $subject, 'message' => $message);
-    header("Location:contact.php?status=thanks");
-}
+    if(!isset($error_message) && !filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $error_message = "Invalid Email Address";
+    }
+    if(!isset($error_message)){
+        $body = array('email' => $email, 'subject' => $subject, 'message' => $message);
 
-function test_input($data){
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+        header("Location:contact.php?status=thanks");
+    }
 }
-
-#print_r($body['message']);
 ?>
-
 <section>
     <h3>Contact Me</h3>
-    <?php
-    if(isset($_GET["status"]) && $_GET["status"]=="thanks"){
-        echo "<br><br>Email sent!  Thanks and I'll get right back with you";
+    <?php if(isset($_GET["status"]) && $_GET["status"] == "thanks"){
+        echo"<p>Thanks for the email! I&rsquo;ll get back with you shortly</p>";
+        echo  $body;
+
     }else{
-        ?>
-        <p>I'd love to discuss my current projects and what I am learning about now. Please feel free to reach out.<br>
-            <span class="error">* All fields required</span></p>
-
-        <form id="contactForm" method="post" action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>">
-            Email: <input type="email" name="email">
-            <span class="error">*<?= $emailErr ?></span>
-            <br><br>
-            Subject: <input type="text" name="subject">
-            <span class="error">*<?= $subjectErr ?></span>
-            <br><br>
-            Message:<br> <textarea rows="10" cols="100" name="message" form="contactForm"></textarea>
-            <span class="error">*<?= $messageErr ?></span>
-            <br><br>
-            <input type="checkbox" name="contact_me_by_fax_only" value="1" style="display: none !important" tabindex="-1" autocomplete="nope">
-            <input type="submit" value="send">
-        </form>
-    </section>
-    <?php
-    include 'includes/connecttoaws.php';
-
-    // Aws sdk parameter
-    try {
-        $result = $client->sendEmail([
-            'Destination' => [
-                'ToAddresses' => ['traymarkthompson@gmail.com']
-            ],
-            'Message' => [
-                'Body' => [
-                    'Text' => [
-                        'Charset' => 'UTF-8',
-                        'Data' => $body['message']
-                    ],
-                ],
-                'Subject' => [
-                    'Charset' => 'UTF-8',
-                    'Data' => $body['subject']
-                ],
-            ],
-            'Source' => 'traymarkthompson@gmail.com'
-        ]);
-        $messageId = $result->get('MessageId');
-
-    }catch (SesException $error) {
-        echo("The email was not sent. Error message: " . $error->getAwsErrorMessage() . "</br>");
+    if(isset($error_message)){
+        echo '<p class="error">' . $error_message . '</p>';
+    }else{
+        echo '<p>I would love to discuss my current projects and what I am learning about now. Please feel free to reach out.  All fields required.</p>';
     }
+    ?>
+    <form method="post" action="contact.php">
+        <table>
+            <tr>
+                <th><label for="email">Email</label></th>
+                <td><input type="text" id="email" name="email" value="<?php if(isset($email)) echo $email;?>"></td>
+            </tr>
+            <tr>
+                <th><label for="subject">Subject</label></th>
+                <td><input type="text" id="subject" name="subject" value="<?php if(isset($subject)) echo htmlspecialchars($_POST["subject"]);?>"></td>
+            </tr>
+            <tr>
+                <th><label for="message">Message</label></th>
+                <td><textarea id="message" name="message" value="<?php if(isset($message)) echo htmlspecialchars($_POST["message"]);?>"></textarea></td>
+            </tr>
+            <tr>
+                <td><input type="checkbox" name="contact_me_by_fax_only" value="1" style="display: none !important" tabindex="-1" autocomplete="nah" /></td>
+            </tr>
+        </table>
+        <input type="submit" value="send"/>
+    </form>
+</section>
+
+<?php
+include 'includes/connecttoaws.php';
+
+// Aws sdk parameter
+try {
+    $result = $client->sendEmail([
+        'Destination' => [
+            'ToAddresses' => ['traymarkthompson@gmail.com']
+        ],
+        'Message' => [
+            'Body' => [
+                'Text' => [
+                    'Charset' => 'UTF-8',
+                    'Data' => $body['message']
+                ],
+            ],
+            'Subject' => [
+                'Charset' => 'UTF-8',
+                'Data' => $body['subject']
+            ],
+        ],
+        'Source' => 'traymarkthompson@gmail.com'
+    ]);
+    $messageId = $result->get('MessageId');
+
+}catch (SesException $error) {
+    echo("The email was not sent. Error message: " . $error->getAwsErrorMessage() . "</br>");
+}
 }
 include 'includes/footer.php' ?>
